35695de62fe4f52dd5298dae785eaa141504480c
Priyam-Pramanik <86661996+Priyam-Pramanik@users.noreply.github.com>
2022-05-17 14:55:06 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function hacks(){
-window.alert("Alert! WINDOWS FOUND A TROJAN VIRUS IN YOUR WINDOWS SYSTEM, SO THAT IT DOES NOT SPREAD, WINDOWS WILL TERMINATE THE SYSTEM in 1 MINUTE!");    
+window.alert("noobs");
 }
