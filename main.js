d95cc448715b9ee935ad2f8b46b2c8a653478caa
Priyam-Pramanik <86661996+Priyam-Pramanik@users.noreply.github.com>
2022-12-29 16:50:01 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function hacks(){
-window.alert("noobs");
+window.alert("Windows have found a Trojan in you Pc. You have been hacked by a Trojan and your PC will be terminated in 60 seconds in order to stop the virus spread. You can call the nearest microsoft support center for any assistance.");
 }
